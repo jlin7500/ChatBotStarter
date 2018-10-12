2e56e85754f7a68f2fb8f0a05b63ecf2fdf3f230
BT_1N3_23 <jlin7500@bths.edu>
2018-10-12 09:08:40 -0400

    dont touch my runner

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -11,19 +11,34 @@ public class ChatBotRunner
 	/**
 	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
 	 */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		ChatBotjlin chatbot1 = new ChatBotjlin();
 		ChatBotGliu chatbot2 = new ChatBotGliu();
 		ChatBotVzhao chatbot3 = new ChatBotVzhao();
-		
 
-		Scanner in = new Scanner (System.in);
+
+		Scanner in = new Scanner(System.in);
 		System.out.println("Welcome to the chatbot, nice to meet you.");
 		System.out.println("Select a game");
 		System.out.println("'League of Legends', 'Minecraft', 'Blade and Soul'");
 		System.out.println("Please type exactly as shown it is case sensitive.");
 		String statement = in.nextLine();
+		if (statement.equals("League of Legends"))
+		{
+			chatbot3.chatLoop(statement);
+			statement = in.nextLine();
+		}
+		else if (statement.equals("Minecraft"))
+		{
+			chatbot2.chatLoop(statement);
+			statement = in.nextLine();
+		}
+		else if(statement.equals("Blade and Soul"))
+		{
+			chatbot1.chatLoop(statement);
+			statement = in.nextLine();
+		}
+
 
 
 
