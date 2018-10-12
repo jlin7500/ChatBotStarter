9ff7cd0dabcf94fe1eba7a1e8ceb8921b9c6d8d9
BT_1N3_22 <vzhao2047@bths.edu>
2018-10-12 10:07:37 -0400

    7

--- src/ChatBotVzhao.java
+++ src/ChatBotVzhao.java
@@ -80,6 +80,10 @@ public class ChatBotVzhao
 		{
 			response = transformIWantStatement(statement);
 		}
+		else if (findKeyword(statement, "I love",0) >= 0)
+		{
+			response = transformIYouStatement(statement);
+		}
 		else
 		{
 			response = getRandomResponse();
@@ -157,6 +161,21 @@ public class ChatBotVzhao
 		
 		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
 		return "Why do you " + restOfStatement + " me?";
+	}
+	private String transformILovetatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I Love", 0);
+		String restOfStatement = statement.substring(psn + 4).trim();
+		return "I Love " + restOfStatement + " also!";
 	}
 	
 
