afaeecd7228d79a8641c08a35ac107b48516d4fa
Jonathan <jlin7500@bths.edu>
2018-10-12 21:43:08 -0400

    big brain

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -21,21 +21,31 @@ public class ChatBotRunner
 		System.out.println("Welcome to the chatbot, nice to meet you.");
 		System.out.println("Select a game");
 		System.out.println("'League of Legends', 'Minecraft', 'Blade and Soul'");
-		System.out.println("Please type exactly as shown it is case sensitive.");
+		System.out.println("Please type exactly as shown as the bot is case sensitive.");
 		String statement = in.nextLine();
+<<<<<<< Updated upstream
 		double noChoice = Math.random() * 3 + 1;
 		int noChoice1 = (int)noChoice;
 		if (statement.equalsIgnoreCase("League of Legends"))
+=======
+		if (statement.equals("League of Legends"))
+>>>>>>> Stashed changes
 		{
 			chatbot3.chatLoop(statement);
 			statement = in.nextLine();
 		}
+<<<<<<< Updated upstream
 		else if (statement.equalsIgnoreCase("Minecraft"))
 		{
 			chatbot2.chatLoop(statement);
 			statement = in.nextLine();
 		}
 		else if(statement.equalsIgnoreCase("Blade and Soul"))
+=======
+
+		if (statement.equals(""))
+		while (!statement.equals("Bye"))
+>>>>>>> Stashed changes
 		{
 			chatbot1.chatLoop(statement);
 			statement = in.nextLine();
